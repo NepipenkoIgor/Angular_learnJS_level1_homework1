5e48101082e5354d0d395469ca147ca03b18b555
Igr <igrflex@gmail.com>
2018-04-24 16:07:20 +0300

    Added start components and copied design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { WallpaperComponent } from './wallpaper/wallpaper.component';
+import { SocialInfoComponent } from './social-info/social-info.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    WallpaperComponent,
+    SocialInfoComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule
